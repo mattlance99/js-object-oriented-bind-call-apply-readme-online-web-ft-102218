be787a554d654e524c90f96a88963cea0b129e33
github username <github email address>
2019-02-26 16:24:36 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// use this to copy code snippets or use your browser's console
+//use this to copy code snippets or use your browser's console
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
